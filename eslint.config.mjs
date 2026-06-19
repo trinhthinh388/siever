0fd92dfd9ab4d3b803d4c0ddd443dca5e6d3a3fe
2t1d <thinh.duc.trinh@gmail.com>
2026-06-19 20:08:31 +0700

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,102 @@
+import nx from '@nx/eslint-plugin';
+import perfectionist from 'eslint-plugin-perfectionist';
+import preferArrowFunctions from 'eslint-plugin-prefer-arrow-functions';
+import prettierPlugin from 'eslint-plugin-prettier/recommended';
+
+export default [
+  ...nx.configs['flat/base'],
+  ...nx.configs['flat/typescript'],
+  ...nx.configs['flat/javascript'],
+  {
+    ignores: ['**/dist', '**/out-tsc', '**/vite.config.*.timestamp*'],
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    rules: {
+      '@typescript-eslint/no-namespace': 'off',
+      '@typescript-eslint/method-signature-style': ['warn', 'property'],
+      '@nx/enforce-module-boundaries': [
+        'error',
+        {
+          enforceBuildableLibDependency: true,
+          allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?[jt]s$'],
+          depConstraints: [
+            {
+              sourceTag: '*',
+              onlyDependOnLibsWithTags: ['*'],
+            },
+          ],
+        },
+      ],
+    },
+  },
+  {
+    plugins: {
+      perfectionist,
+      'prefer-arrow-functions': preferArrowFunctions,
+    },
+    files: [
+      '**/*.ts',
+      '**/*.tsx',
+      '**/*.cts',
+      '**/*.mts',
+      '**/*.js',
+      '**/*.jsx',
+      '**/*.cjs',
+      '**/*.mjs',
+    ],
+    // Override or add rules here
+    rules: {
+      'perfectionist/sort-objects': [
+        'error',
+        {
+          order: 'asc',
+          type: 'line-length',
+        },
+      ],
+      'perfectionist/sort-interfaces': [
+        'error',
+        {
+          order: 'asc',
+          type: 'line-length',
+        },
+      ],
+      'perfectionist/sort-object-types': [
+        'error',
+        {
+          order: 'asc',
+          type: 'line-length',
+        },
+      ],
+      'prefer-arrow-functions/prefer-arrow-functions': [
+        'warn',
+        {
+          disallowPrototype: true,
+          classPropertiesAllowed: true,
+        },
+      ],
+      'perfectionist/sort-classes': [
+        'error',
+        {
+          order: 'asc',
+          type: 'line-length',
+          groups: [
+            'index-signature',
+            'static-property',
+            'private-property',
+            'protected-property',
+            'property',
+            'constructor',
+            'static-method',
+            'method',
+            'protected-method',
+            'private-method',
+            ['get-method', 'set-method'],
+            'unknown',
+          ],
+        },
+      ],
+    },
+  },
+  prettierPlugin,
+];
---
+++ packages/core/eslint.config.mjs
@@ -0,0 +1,22 @@
+import baseConfig from '../../eslint.config.mjs';
+
+export default [
+  ...baseConfig,
+  {
+    files: ['**/*.json'],
+    languageOptions: {
+      parser: await import('jsonc-eslint-parser'),
+    },
+    rules: {
+      '@nx/dependency-checks': [
+        'error',
+        {
+          ignoredFiles: [
+            '{projectRoot}/eslint.config.{js,cjs,mjs,ts,cts,mts}',
+            '{projectRoot}/vite.config.{js,ts,mjs,mts}',
+          ],
+        },
+      ],
+    },
+  },
+];
